d2021eb8790439775c1b75cbb691fb61259528da
Stephen Richard <steve.richard@ldeo.columbia.edu>
2017-09-10 11:32:14 -0400

    add files related to ECL metadata operations

---
+++ EarthChemLibrary/archiveJobJSON-fromGRLdb.sql
@@ -0,0 +1,72 @@
+﻿--SQL query to generate a JSON archive job description to feed bulk archvie update process
+--  for IEDA
+-- Stephen M. Richard
+--  2017-03-30
+
+select s.submission_id, 
+
+
+
+(select row_to_json(aaa) from (
+    select 
+  s.title as "itemName",
+  translate(trim((date_trunc('second', current_timestamp(0)))::text),' ', 'T') as "jobSubmissionDate",
+ -- array of identifiers, each with a 'type'; 'DOI' should be one of these, 
+ -- if not some other globally unique id should be included, here the second option
+ -- is a urn based on the primary key of the submissions table in the EarthChemLibrary 
+ -- database
+  (select array_to_json(
+	array[(
+		select row_to_json(xx) from
+			(select 'DOI' as "identifierType",
+				s.dataset_doi as identifier 
+			)  xx
+		) ,
+		(select row_to_json(yy) from
+			(select 'IEDA submission_ID' as "identifierType",
+				concat('urn:grl:submission:', s.submission_ID) as identifier 
+			) yy)
+		]
+	)) as identifiers,
+-- provide format (e.g. ISO19139, DataCite v4, using the xml schema namespace URI if its xml) and
+-- a  path to retrieve the metadata record documenting the repository item.
+	(select row_to_json(zz) from 
+		(select 'http://datacite.org/schema/kernel-4'::text as format,
+			concat('metadata/',
+				(case
+					when (s.dataset_doi != '') then
+					concat(right(trim(leading 'doi:'::text from s.dataset_doi),6),'-'::text)
+					end),
+				'submission-id'::text ,trim(to_char(s.submission_id,'0000')),
+				'.xml'::text
+			) as "metadataFileLocation" 
+		)  zz
+	) as "metadataLink",
+-- construct array of repository content items to load into the archive object; each has a path for locating
+-- the content items; this can be local file paths the processor will understand, or a URL
+    (
+      select array_to_json(array_agg(row_to_json(d)))
+		from (
+			SELECT 
+				concat('%path%/submissions/in/'::text,s.submission_id,'/'::text, dfl.dataset_server_filename) "fileLocation", 
+				concat(dff.data_file_format, ': ', dff.data_file_format_name) format
+			from
+			  dataset_file_list dfl, 
+			data_file_formats dff
+				where   s.submission_id = dfl.submission_id AND
+				dfl.data_file_format_id = dff.data_file_format_id
+			) d
+		) as "contentItems",
+		''::text as "archiveAccessionDate",
+		''::text as "archiveID",
+		'EarthChem Library'::text as "sourceSystem",
+		'getFile-dcv4-tgzbagit-CUL'::text as "archiveWorkflow"
+
+		) as aaa
+	) as "archiveJob"
+  FROM
+  submissions s
+
+WHERE 
+-- only take published repository items, indicated by status_id=2
+   s.status_id=2 ;
---
+++ EarthChemLibrary/grl_dump2meta.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python2.6
+
+"""
+Author: Neville Shane
+Institution: LDEO, Columbia University
+Email: nshane@ldeo.columbia.edu
+Creation date: March 29 2017
+Usage: grl_dump2meta.py -d <dump_file> -o <output_dir>
+This program reads a csv file with two columns:
+submission_id -- contains a unique record identifier integer (e.g.
+    a submission number from EarthChem Library)
+dataCiteXML-a text blob that is valid xml (without the xml version header)
+    for DataCitev.4 encoded metadata describing a resource.
+Each xml file is saved with the identifiers for the resource in the file name.
+
+To run, type:
+> ./grl_dump2meta.py -d <dump_file> -o <output_dir>
+This will create the output directory specified by the -o argument and
+write the dataCite metadata files to that location. 
+
+updates:
+2017-03-30 SMR added more header information; put the main loop process in
+    a try/except block
+"""
+
+import sys
+import os
+import csv
+import getopt
+import xml.etree.ElementTree as ET
+
+XML_HEADER = '<?xml version="1.0" encoding="UTF-8"?>'
+ns = "{http://datacite.org/schema/kernel-4}"
+# get the dump_file and output_dir from the input arguements
+dump_file = None
+output_dir = None
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hd:o:", ["dump_file=", "output_dir="])
+    if len(opts) == 0:
+        print("grl_dump2meta.py -d <dump_file> -o <output_dir>")
+        sys.exit(0)
+except getopt.GetoptError:
+    print("grl_dump2meta.py -d <dump_file> -o <output_dir>")
+    sys.exit(0)
+for opt, arg in opts:
+    if opt == '-h':
+        print("grl_dump2meta.py -d <dump_file> -o <output_dir>")
+        sys.exit(0)
+    elif opt in ('-d', '--dump_file'):
+        dump_file = arg
+    elif opt in ('-o', '--output_dir'):
+        output_dir = arg
+if dump_file is None or output_dir is None:
+        print("grl_dump2meta.py -d <dump_file> -o <output_dir>")
+        sys.exit(0)
+
+# create output_dir if it doesn't already exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# read in dump_file
+if not os.path.exists:
+    print("ERROR: dump file %s not found" % dump_file)
+    sys.exit(0)
+
+# store submission in a dictionary, where the submission_id is the key
+submissions = {}
+try:
+    with open(dump_file, 'rt') as f:
+        reader = csv.reader(f, delimiter='|')
+        header = True
+        for row in reader:
+            # skip header line
+            if header:
+                header = False
+                continue
+            # next submission object of the row contains 2 objects
+            if len(row) == 2:
+                # split submission_id and dataCiteXML
+                sub_id = row[0]
+                xml = row[1]
+            # if just one object - it's more xml
+            elif len(row) == 1:
+                # build up the xml by appending lines to it
+                xml += "\n" + row[0]
+                submissions[sub_id] = xml
+except:
+    print("ERROR: Can't read dump_file: " + dump_file + "; row " + sub_id)
+    print(sys.exc_info()[1])
+    sys.exit(0)
+
+# for each submission
+
+for sub_id, xml in submissions.items():
+    try:
+        # generate metadata file name
+        # first parse in the xml
+        if len(sub_id)== 0:
+            print("No sub_id provided, skip")
+            continue
+        else:
+            print('{:04d}'.format(int(sub_id)))
+        root = ET.fromstring(xml)
+        identifier = root.find(ns+'identifier')
+        if identifier is not None:
+            # use the DOI in the filename is available
+            if identifier.get('identifierType') == "DOI":
+                doi = identifier.text.split('/')
+                meta_name = doi[2] + "-submission-id" + '{:04d}'.format(int(sub_id)) + ".xml"
+            else:
+                meta_name = "submission-id" + '{:04d}'.format(int(sub_id)) + ".xml"
+        else:
+            print("identifier missing: " + '{:04d}'.format(int(sub_id)))
+            continue
+
+        meta_file = os.path.join(output_dir, meta_name)
+
+        # save dataCiteXML into metadata file
+        outfile = open(meta_file, 'w')
+        # add XML header
+        outfile.write(XML_HEADER)
+        outfile.write("\n")
+        outfile.write(xml)
+        outfile.close()
+##        print("INFO: metadata file written: %s" % meta_file)
+    except:
+        print("EXCEPTION: error on submission: sub_ID=" + '{:04d}'.format(int(sub_id)))
+        if outfile:
+            outfile.close()
+
+print("DONE")
